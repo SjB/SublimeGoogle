ffe43f3ed82de7747d61b4f94e2cdca9d0cf20d3
SjB <steve@sagacity.ca>
2013-10-09 12:15:14 -0400

    Added ddg category options to search queries

--- SublimeGoogle.py
+++ SublimeGoogle.py
@@ -5,17 +5,22 @@ class GoogleSearchSelectionCommand(sublime_plugin.WindowCommand):
 	"""
 	Search selected text
 	"""
-	def google_search(self,text):
+	def google_search(self, text):
 		"""
 		Search text in google and place results in self.results, then show in quick panel
 		"""
 		sublime.status_message("I\'ve opened web browser for this page.")
 		webbrowser.open("http://www.duckduckgoog.com/?q="+text)
 		return 0
-	def run(self):
+
+	def run(self, category=None):
 		v = self.window.active_view()
 		text = v.substr(v.sel()[0]).strip()
 		self.results=list()
+		if category:
+			text = "!{0} {1}".format(category, text)
+
+		sublime.error_message(text)
 		self.google_search(text)
 
 		return 0
